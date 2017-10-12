f4632878d45da9f4db90b94dfbf483a9838bc134
cliffgor <clierozigy@gmail.com>
2017-10-12 11:26:45 +0300

    Createed a model class inside the nodels package

---
+++ app/src/main/java/com/example/cliff/chatme/Models/ChatMeModel.java
@@ -0,0 +1,15 @@
+package com.example.cliff.chatme.Models;
+
+/**
+ * Created by cliff on 10/12/2017.
+ */
+
+public class ChatMeModel {
+    public String response;
+    public String id;
+    public int result;
+    public String msg;
+
+
+
+}
