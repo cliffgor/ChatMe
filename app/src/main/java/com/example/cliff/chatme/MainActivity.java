76460cb7a5813ea503eeb8431dc906200cdc0c50
cliffgor <clierozigy@gmail.com>
2017-10-12 13:17:47 +0300

    ChatModel; *added getters and setters *added class properties with a oolean to check if a message is sent

--- app/src/main/java/com/example/cliff/chatme/MainActivity.java
+++ app/src/main/java/com/example/cliff/chatme/MainActivity.java
@@ -6,17 +6,12 @@ import android.os.Bundle;
 import ai.api.AIListener;
 import ai.api.android.AIConfiguration;
 import ai.api.android.AIService;
-import ai.api.model.AIError;
-import ai.api.model.AIResponse;
-import ai.api.model.Result;
-import com.google.gson.JsonElement;
-import java.util.Map;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity implements AIListener {
+public class MainActivity extends ActionBarActivity {
     private Button listenButton;
     private TextView resultTextView;
     private AIService aiService;
@@ -32,46 +27,46 @@ public class MainActivity extends ActionBarActivity implements AIListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listenButton = (Button) findViewById(R.id.listenButton);
-        resultTextView = (TextView) findViewById(R.id.resultTextView);
-
-        final AIConfiguration config = new AIConfiguration("CLIENT_ACCESS_TOKEN",
-                AIConfiguration.SupportedLanguages.English,
-                AIConfiguration.RecognitionEngine.System);
-
-        aiService = AIService.getService(this, config);
-        aiService.setListener(this);
+//        listenButton = (Button) findViewById(R.id.listenButton);
+//        resultTextView = (TextView) findViewById(R.id.resultTextView);
+//
+//        final AIConfiguration config = new AIConfiguration("CLIENT_ACCESS_TOKEN",
+//                AIConfiguration.SupportedLanguages.English,
+//                AIConfiguration.RecognitionEngine.System);
+//
+//        aiService = AIService.getService(this, config);
+//        aiService.setListener(this);
     }
-
-    @Override
-    public void onResult(ai.api.model.AIResponse result) {
-
-    }
-
-    @Override
-    public void onError(ai.api.model.AIError error) {
-
-    }
-
-    @Override
-    public void onAudioLevel(float level) {
-
-    }
-
-    @Override
-    public void onListeningStarted() {
-
-    }
-
-    @Override
-    public void onListeningCanceled() {
-
-    }
-
-    @Override
-    public void onListeningFinished() {
-
-    }
+//
+//    @Override
+//    public void onResult(ai.api.model.AIResponse result) {
+//
+//    }
+//
+//    @Override
+//    public void onError(ai.api.model.AIError error) {
+//
+//    }
+//
+//    @Override
+//    public void onAudioLevel(float level) {
+//
+//    }
+//
+//    @Override
+//    public void onListeningStarted() {
+//
+//    }
+//
+//    @Override
+//    public void onListeningCanceled() {
+//
+//    }
+//
+//    @Override
+//    public void onListeningFinished() {
+//
+//    }
 
 //    public void onResult(final AIResponse response) {
 //        Result result = response.getResult();
