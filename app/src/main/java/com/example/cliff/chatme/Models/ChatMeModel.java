e02220eedf31e6e612ae19a5e4d85caf5ba7c5f4
cliffgor <clierozigy@gmail.com>
2017-10-12 11:57:11 +0300

    ChatMeModel-class; *added the class properties *added  constractor class *added setters and getters

--- app/src/main/java/com/example/cliff/chatme/Models/ChatMeModel.java
+++ app/src/main/java/com/example/cliff/chatme/Models/ChatMeModel.java
@@ -11,5 +11,46 @@ public class ChatMeModel {
     public String msg;
 
 
+    public ChatMeModel(String response, String id, int result, String msg) {
+        this.response = response;
+        this.id = id;
+        this.result = result;
+        this.msg = msg;
+    }
 
+    public ChatMeModel(String response) {
+        this.response = response;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 }
