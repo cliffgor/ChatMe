5de6502bbacdeff27ad8595754b4d55e703ac672
cliffgor <clierozigy@gmail.com>
2017-10-13 16:19:23 +0300

    Adapters; *Created a custom adapters

---
+++ app/src/main/java/com/example/cliff/chatme/Adapter/CustomAdapter.java
@@ -0,0 +1,37 @@
+package com.example.cliff.chatme.Adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by cliff on 10/13/2017.
+ */
+
+public class CustomAdapter extends BaseAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public CharSequence[] getAutofillOptions() {
+        return new CharSequence[0];
+    }
+}
